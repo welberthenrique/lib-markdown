f362aa5abb8624b1c38f5b2a77786f4504f81430
welberthenrique <welberthenrique@gmail.com>
2022-02-21 23:33:07 -0300

    Utilizando promises

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import chalk from "chalk";
 import * as fs from "fs";
+const log = console.log;
 
 function tratarErro(erro) {
   throw new Error(chalk.red(erro.code, "Não há arquivo no caminho."));
@@ -7,12 +8,20 @@ function tratarErro(erro) {
 
 function pegaArquigo(caminhoDoArquivo) {
   const encoding = "utf-8";
-  fs.readFile(caminhoDoArquivo, encoding, (erro, texto) => {
-    if (erro) {
-      tratarErro(erro);
-    }
-    console.log(chalk.green(texto));
-  });
+  fs.promises
+    .readFile(caminhoDoArquivo, encoding)
+    .then((texto) => log(chalk.green(texto)))
+    .catch((erro) => tratarErro(erro));
 }
+
+// function pegaArquigo(caminhoDoArquivo) {
+//   const encoding = "utf-8";
+//   fs.readFile(caminhoDoArquivo, encoding, (erro, texto) => {
+//     if (erro) {
+//       tratarErro(erro);
+//     }
+//     console.log(chalk.green(texto));
+//   });
+// }
 
 pegaArquigo("./arquivos/texto1.md");
