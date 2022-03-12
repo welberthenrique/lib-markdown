b33d3b806203112eaf388efed77d1ad97e85142e
welberthenrique <welberthenrique@gmail.com>
2022-03-12 15:36:19 -0300

    Adicionado validação de url com status code

--- cli.js
+++ cli.js
@@ -1,11 +1,16 @@
 import chalk from "chalk";
 import pegaArquivo from "./index.js";
+import validaURLs from "./http-validacao.js";
 
 const caminho = process.argv;
 
 async function processaTexto(caminhoDeArqivo) {
   const resultado = await pegaArquivo(caminhoDeArqivo[2]);
-  console.log(chalk.yellow("lista de links"), resultado);
+  if (caminho[3] === "validar") {
+    console.log(chalk.yellow("links validados"), await validaURLs(resultado));
+  } else {
+    console.log(chalk.yellow("lista de links"), resultado);
+  }
 }
 
 processaTexto(caminho);
--- http-validacao.js
+++ http-validacao.js
@@ -1,11 +1,27 @@
+import fetch from "node-fetch";
+
+async function checaStatus(arrayURLs) {
+  //promise async await
+  const arrayStatus = await Promise.all(
+    arrayURLs.map(async (url) => {
+      const res = await fetch(url);
+      return res.status;
+    })
+  );
+  return arrayStatus;
+}
+
 function geraArrayDeURLs(arrayLinks) {
   //loop para cada objeto {chave:valor}
   //pegar o objeto e retornar um array apenas com o valor
   //Object.values(objeto);
+  return arrayLinks.map((objetoLink) => Object.values(objetoLink).join());
 }
 
-function validaURLs(arrayLinks) {
-  return arrayLinks;
+async function validaURLs(arrayLinks) {
+  const links = geraArrayDeURLs(arrayLinks);
+  const statusLinks = await checaStatus(links);
+  return statusLinks;
 }
 
 export default validaURLs;
