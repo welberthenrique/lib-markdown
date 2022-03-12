0ee4170f2bbe9180d74d4520b3bc814dd27239ff
welberthenrique <welberthenrique@gmail.com>
2022-03-12 14:08:44 -0300

    Adicionado arquivo para validação http

---
+++ http-validacao.js
@@ -0,0 +1,11 @@
+function geraArrayDeURLs(arrayLinks) {
+  //loop para cada objeto {chave:valor}
+  //pegar o objeto e retornar um array apenas com o valor
+  //Object.values(objeto);
+}
+
+function validaURLs(arrayLinks) {
+  return arrayLinks;
+}
+
+export default validaURLs;
