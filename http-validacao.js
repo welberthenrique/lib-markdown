5279b9e7a34ca778adde2c588b0a571cab0274de
welberthenrique <welberthenrique@gmail.com>
2022-03-12 16:17:42 -0300

    Adicionado validação dos links com status code e retorno do link com status

--- http-validacao.js
+++ http-validacao.js
@@ -1,14 +1,22 @@
 import fetch from "node-fetch";
 
+function manejaErros(erro) {
+  throw new Error(erro.message);
+}
+
 async function checaStatus(arrayURLs) {
   //promise async await
-  const arrayStatus = await Promise.all(
-    arrayURLs.map(async (url) => {
-      const res = await fetch(url);
-      return res.status;
-    })
-  );
-  return arrayStatus;
+  try {
+    const arrayStatus = await Promise.all(
+      arrayURLs.map(async (url) => {
+        const res = await fetch(url);
+        return `${res.status} - ${res.statusText}`;
+      })
+    );
+    return arrayStatus;
+  } catch (erro) {
+    manejaErros(erro);
+  }
 }
 
 function geraArrayDeURLs(arrayLinks) {
@@ -21,7 +29,12 @@ function geraArrayDeURLs(arrayLinks) {
 async function validaURLs(arrayLinks) {
   const links = geraArrayDeURLs(arrayLinks);
   const statusLinks = await checaStatus(links);
-  return statusLinks;
+  //console.log(arrayLinks, statusLinks);
+  const resultados = arrayLinks.map((objeto, indice) => ({
+    ...objeto,
+    status: statusLinks[indice],
+  }));
+  return resultados;
 }
 
 export default validaURLs;
